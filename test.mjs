7d17e43d623db9cc88c92cf257aa7f4b4a7cdb8e
carys <caryspritch@gmail.com>
2022-07-12 21:17:41 +0100

    display json data

--- test.mjs
+++ test.mjs
@@ -1,10 +1,33 @@
 import {myjson} from './csv.mjs' 
 
-function myfunc(){
-console.log(myjson[1].HotelName)
-// document.getElementById("demo").innerHTML = myjson[1].HotelName;
-
-
+function africa(){
+document.getElementById("africa").innerHTML = myjson[3].Continent;
 }
 
-export {myfunc};
+function asia(){
+    document.getElementById("asia").innerHTML = myjson[0].Continent;
+    }
+
+function aus(){
+    document.getElementById("aus").innerHTML = myjson[4].Continent;
+    }
+function northA(){
+    document.getElementById("northA").innerHTML = myjson[1].Continent;
+    }
+function eur(){
+    document.getElementById("eur").innerHTML = myjson[2].Continent;
+    }
+function ant(){
+    document.getElementById("ant").innerHTML = myjson[19].Continent;
+    }
+function arc(){
+    document.getElementById("arc").innerHTML = myjson[20].Continent;
+    }
+
+export {africa};
+export {asia};
+export {aus};
+export {northA};
+export {eur};
+export {ant};
+export {arc};
