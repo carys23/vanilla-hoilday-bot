12840099b1fd20ef50d3f7c138fcc9430cf7d07b
carys <caryspritch@gmail.com>
2022-07-12 17:33:52 +0100

    first commit

---
+++ test.mjs
@@ -0,0 +1,10 @@
+import {myjson} from './csv.mjs' 
+
+function myfunc(){
+console.log(myjson[1].HotelName)
+// document.getElementById("demo").innerHTML = myjson[1].HotelName;
+
+
+}
+
+export {myfunc};
