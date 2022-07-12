85dbfe0e3cb1dbcedd34bb70bd8f7c5e0e3b1a9e
carys <caryspritch@gmail.com>
2022-07-12 23:32:04 +0100

    added event buttons

--- test.mjs
+++ test.mjs
@@ -2,27 +2,97 @@ import {myjson} from './csv.mjs'
 
 function africa(){
 document.getElementById("africa").innerHTML = myjson[3].Continent;
+document.getElementById("africa").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("africa").innerHTML = myjson[3].Area;
 }
+}
+
+
 
 function asia(){
     document.getElementById("asia").innerHTML = myjson[0].Continent;
+    document.getElementById("asia").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("asia").innerHTML = myjson[3].Area;
+}
     }
 
 function aus(){
     document.getElementById("aus").innerHTML = myjson[4].Continent;
+    document.getElementById("aus").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("aus").innerHTML = myjson[3].Area;
+}
     }
 function northA(){
     document.getElementById("northA").innerHTML = myjson[1].Continent;
+    document.getElementById("northA").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("northA").innerHTML = myjson[3].Area;
+}
     }
+
 function eur(){
     document.getElementById("eur").innerHTML = myjson[2].Continent;
+    document.getElementById("eur").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("eur").innerHTML = myjson[3].Area;
+}
     }
 function ant(){
     document.getElementById("ant").innerHTML = myjson[19].Continent;
+    document.getElementById("ant").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("ant").innerHTML = myjson[3].Area;
+}
     }
 function arc(){
     document.getElementById("arc").innerHTML = myjson[20].Continent;
+    document.getElementById("arc").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("arc").innerHTML = myjson[3].Area;
+}
     }
+// function africaOp(){
+//     console.log(myjson[3].Area)
+//     document.getElementById("africa").innerHTML = myjson[3].Area;
+// }
+// function asiaOp(){
+//     console.log(myjson[3].Area)
+//     document.getElementById("africaOp").innerHTML = myjson[3].Area;
+// }
+// function ausOp(){
+//     console.log(myjson[3].Area)
+//     document.getElementById("africaOp").innerHTML = myjson[3].Area;
+// }
+// function northAOp(){
+//     console.log(myjson[3].Area)
+//     document.getElementById("africaOp").innerHTML = myjson[3].Area;
+// }
+// function eurOp(){
+//     console.log(myjson[3].Area)
+//     document.getElementById("africaOp").innerHTML = myjson[3].Area;
+// }
+
+// function arcOp(){
+//     console.log(myjson[3].Area)
+//     document.getElementById("africaOp").innerHTML = myjson[3].Area;
+// }
+
+// function antOp(){
+//     console.log(myjson[3].Area)
+//     document.getElementById("africaOp").innerHTML = myjson[3].Area;
+// }
+
+
 
 export {africa};
 export {asia};
@@ -31,3 +101,12 @@ export {northA};
 export {eur};
 export {ant};
 export {arc};
+
+
+// export {asiaOp};
+// export {ausOp};
+// export {northAOp};
+// export {eurOp};
+// export {antOp};
+// export {arcOp};
+// export {africaOp};
